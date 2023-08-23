1f126836c84ed60cbfb4e215e23f1149c85b4403
Kate <vesnavved@gmail.com>
2023-08-23 19:53:31 +0300

    feat: + project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+import { Loader } from './components/loader'
 import './App.css'
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <Loader/>
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -23,7 +24,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/loader/index.jsx
@@ -0,0 +1,9 @@
+import "./loader.scss";
+
+
+
+export const Loader = () => (
+  <div className="pos-center">
+    <div data-test-id="loader" className="loader"></div>
+  </div>
+);
